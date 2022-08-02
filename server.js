6e9f9dd90137179a811e84ddfd81dff41a6d2324
darmstrong <armstrong2579@gmail.com>
2022-08-02 16:43:37 -0700

    add authorization function

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const path = require('path');
+const mysql = require('mysql2');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers: helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
