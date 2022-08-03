0ef1e4f92b678b5146990292be53ffa33ae26a92
darmstrong <armstrong2579@gmail.com>
2022-08-02 19:43:11 -0700

    add homepage and login routes, comments for others needed

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,40 @@
+// login 
+// routes for display
+
+// -------NEED--------
+// can get posts by id (display page taking the information needed)
+// comments (need to be displayed with posts when existing)
+
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Route to display the login page -- directs to homepage if user is already logged in
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
