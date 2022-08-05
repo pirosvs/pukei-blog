893f1f50f58d8a68db6ddb38f5dc758ea0c67072
darmstrong <armstrong2579@gmail.com>
2022-08-04 22:23:41 -0700

    add to comment routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,2 +1,32 @@
-// get blog by user id
-// get comments taking blog post it
+// get blog by user id -- in the dashboard?? where is this even done
+// get comments taking blog post id
+
+const router = require("../homeRoutes");
+
+router.get('/blog', async (req, res) => {
+    
+});
+
+// Should get all comments related to a given blogpost
+router.get('/comment', async (req, res) => {
+    const commentData = Comment.findAll({
+        where: {post_id: blogPostData.id}
+    });
+});
+
+// Should delete a selected comment
+router.delete('/comment/:id', async (req, res) => {
+   try {
+    const commentData = Comment.destroy({
+        where: {id: req.params.id}
+    });
+    if (!commentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+    }
+  
+    res.status(200).json(commentData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
